6d6d97e2d64f11440fdc46c2ae71b7106a1bed6f
Jan <kupper.thorge@gmail.com>
2025-07-03 11:13:17 +0200

    day3 done

--- week1-typescript-fundamentals/day3-typescript-react/src/app/page.tsx
+++ week1-typescript-fundamentals/day3-typescript-react/src/app/page.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import TodoList from '@/components/TodoList';
-import Counter from '../components/Counter';
-import UserCard from '../components/UserCard';
 
 export default function Home() {
   return (
     <div className='p-4'>
-      <UserCard />
-      <Counter />
+      {/* <UserCard /> */}
+      {/* <Counter /> */}
       <TodoList />
     </div>
   );
--- week1-typescript-fundamentals/day3-typescript-react/src/components/TodoList.tsx
+++ week1-typescript-fundamentals/day3-typescript-react/src/components/TodoList.tsx
@@ -8,13 +8,44 @@ interface Todo {
 
 export default function TodoList() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [textInput, setTextInput] = useState('');
+  const [isLoaded, setIsLoaded] = useState(false);
 
+  // Setting document title based on todo tasks amount
   useEffect(() => {
-    addTodo('Mow the lawn');
+    const count = todos.length;
+    if (count === 0) {
+      document.title = 'All done!';
+    } else {
+      document.title = `${count} task${count > 1 ? 's' : ''} to do!`;
+    }
+  }, [todos]);
+
+  // Load todos from localstorage
+  useEffect(() => {
+    try {
+      const storedTodos = localStorage.getItem('todos');
+      if (storedTodos) {
+        setTodos(JSON.parse(storedTodos));
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoaded(true);
+    }
   }, []);
 
-  function addTodo(text: string) {
-    setTodos([...todos, { id: Date.now(), text, completed: false }]);
+  // Save todos to localstorage
+  useEffect(() => {
+    if (isLoaded) {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
+  }, [todos, isLoaded]);
+
+  function addTodo() {
+    if (!textInput.trim()) return;
+    setTodos([...todos, { id: Date.now(), text: textInput, completed: false }]);
+    setTextInput('');
   }
 
   function toggleCompleted(id: number) {
@@ -23,19 +54,44 @@ export default function TodoList() {
     );
   }
 
+  function deleteTodo(id: number) {
+    const filteredTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(filteredTodos);
+  }
+
   return (
     <div>
-      {todos.map((todo) => (
-        <div className='flex items-center gap-1' key={todo.id}>
-          <p>{todo.text}</p>
-          <p
-            onClick={() => toggleCompleted(todo.id)}
-            className='text-lg font-mono cursor-default select-none'
-          >
-            {todo.completed ? '■' : '□'}
-          </p>
-        </div>
-      ))}
+      <div className='flex gap-2'>
+        <input
+          type='text'
+          name='todo-input'
+          id='todo-input'
+          className='border'
+          onChange={(e) => setTextInput(e.target.value)}
+          value={textInput}
+        />
+        <button className='border px-1' onClick={addTodo}>
+          Add
+        </button>
+      </div>
+      <div className='max-w-lg w-full flex flex-col gap-2'>
+        {todos.map((todo) => (
+          <div className='flex items-center justify-between' key={todo.id}>
+            <p>{todo.text}</p>
+            <div className='flex gap-2 items-center'>
+              <p
+                onClick={() => toggleCompleted(todo.id)}
+                className='text-xl font-mono cursor-default select-none w-4 h-4 border flex items-center justify-center'
+              >
+                {todo.completed ? '✓' : ''}
+              </p>
+              <button className='border px-1' onClick={() => deleteTodo(todo.id)}>
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
